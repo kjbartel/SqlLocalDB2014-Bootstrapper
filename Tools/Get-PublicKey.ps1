f1effef120f1f11edddd37ccb68a9ff4151b94c6
Kuan Bartel <k.bartel@aist.go.jp>
2015-08-04 14:29:12 +0900

    Added bootstrapper package for SQL Server 2014 SP1 Express LocalDB.

---
+++ Tools/Get-PublicKey.ps1
@@ -0,0 +1,18 @@
+﻿param([Parameter(Mandatory=$True)][string]$msifile = $args[0])
+if (!$msifile)
+{
+    Return
+}
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2((Get-Item $msifile).FullName)
+if ($cert)
+{
+    $key = $cert.GetPublicKeyString()
+    if ($key)
+    {
+        Write-Output $key
+    }
+    else
+    {
+        Write-Host "No public key"
+    }
+}
